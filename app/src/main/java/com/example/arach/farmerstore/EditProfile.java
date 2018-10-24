56a6d6be73e53c5d8b7e84f3efc6e3c0d6f1efc3
Arada <arachpsc115@gmail.com>
2018-10-24 15:59:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/arach/farmerstore/EditProfile.java
@@ -0,0 +1,13 @@
+package com.example.arach.farmerstore;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+public class EditProfile extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editprofile);
+    }
+}
