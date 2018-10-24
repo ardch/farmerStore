ddbc6ac2480230f0b8ef92b502884b6a50b47084
Arada <arachpsc115@gmail.com>
2018-10-24 21:50:28 +0700

    Add SelleSetting.java

---
+++ app/src/main/java/com/example/arach/farmerstore/SellerSetting.java
@@ -0,0 +1,12 @@
+package com.example.arach.farmerstore;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SellerSetting extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(sellersetting);
+    }
+}
